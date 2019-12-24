1d76070b580af4b4d3d82deeb719c81dbf0f91d2
Krasavinigor <48414875+Krasavinigor@users.noreply.github.com>
2019-12-24 19:34:53 +0300

    Add style for main page (#7)
    * Delete extra info
    
    * Add style
    
    * Fix code

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,31 @@
+import Button from '@material-ui/core/Button';
+import { NavLink } from 'react-router-dom';
 import React from 'react';
 
 const home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-      <p>Home page body content</p>
-    </div>
+    <>
+      <div className='wrapper'>
+        <div className='blackLayoutLinear'></div>
+        <div
+          id='qwe'
+          className='wrapper_content'
+        >
+          <NavLink
+            to='/creationCV'
+            className='list-of-item'
+          >
+            <Button
+              id='bt'
+              variant='contained'
+              color='primary'
+            >
+              Create
+            </Button>
+          </NavLink>
+        </div>
+      </div>
+    </>
   );
 };
 
